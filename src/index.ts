aee2b253364e0056b2b68314a8fec9e26d1284fd
Hubert Matwiejczyk <hmatwiejczyk@gmail.com>
2019-07-22 01:27:42 +0200

    add references to method on User class

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,7 @@
 import { User } from './models/User';
 
+const user = new User({ id: 1, name: 'changed name', age: 666 });
 
+user.on('save', () => console.log(user));
+
+user.save();
--- src/models/User.ts
+++ src/models/User.ts
@@ -1,7 +1,8 @@
 import { Eventing } from './Eventing';
 import { Sync } from './Sync';
 import { baseUrl } from '../environment';
-import { Attributes } from "./Attributes";
+import { Attributes } from './Attributes';
+import { AxiosResponse, AxiosPromise } from 'axios';
 interface UserProps {
   id?: number;
   name?: string;
@@ -11,8 +12,40 @@ export class User {
   public events: Eventing = new Eventing();
   public sync: Sync<UserProps> = new Sync<UserProps>(baseUrl);
   public attributes: Attributes<UserProps>;
-  
+
   constructor(attrs: UserProps) {
     this.attributes = new Attributes<UserProps>(attrs);
+  }
+
+  get on() {
+    return this.events.on;
+  }
+  get trigger() {
+    return this.events.trigger;
+  }
+  get get() {
+    return this.attributes.get;
+  }
+  set(update: UserProps): void {
+    this.attributes.set(update);
+    this.events.trigger('change');
+  }
+  fetch(): void {
+    const id = this.attributes.get('id');
+    if (typeof id !== 'number') {
+      throw new Error('Cannot fetch without an id');
+    }
+    this.sync.fetch(id).then(
+      (response: AxiosResponse): void => {
+        this.set(response.data);
+      }
+    );
+  }
+  save(): void {
+    const data = this.attributes.getAll();
+    this.sync
+      .save(data)
+      .then((response: AxiosResponse): void => this.trigger('save'))
+      .catch(() => this.trigger('error'));
   }
 }
