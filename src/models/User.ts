d83ed593c2decdd4854f59f6bfd9650f49f87e6b
Hubert Matwiejczyk <hmatwiejczyk@gmail.com>
2019-07-22 00:19:01 +0200

    add Attributs class, create generic type which returns given types T[K]

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,6 +1,7 @@
 import { Eventing } from './Eventing';
 import { Sync } from './Sync';
-import { baseUrl } from "../environment";
+import { baseUrl } from '../environment';
+import { Attributes } from "./Attributes";
 interface UserProps {
   id?: number;
   name?: string;
@@ -9,13 +10,9 @@ interface UserProps {
 export class User {
   public events: Eventing = new Eventing();
   public sync: Sync<UserProps> = new Sync<UserProps>(baseUrl);
-
-  constructor(private data: UserProps) {}
-
-  get(propName: string): number | string {
-    return this.data[propName];
-  }
-  set(update: UserProps): void {
-    Object.assign(this.data, update);
+  public attributes: Attributes<UserProps>;
+  
+  constructor(attrs: UserProps) {
+    this.attributes = new Attributes<UserProps>(attrs);
   }
 }
