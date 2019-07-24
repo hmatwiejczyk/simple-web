462c8ac7b05af62fd18dc0d0e58143d3b618c542
Hubert Matwiejczyk <hmatwiejczyk@gmail.com>
2019-07-24 23:23:37 +0200

    add user instance to userForm

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 import { UserForm } from './views/UserForm';
+import { User } from './models/User';
 
-const userFrom = new UserForm(document.getElementById('root'));
+const user = User.createUser({ name: 'NAME', age: 20 });
+const userFrom = new UserForm(document.getElementById('root'), user);
 userFrom.render();
--- src/views/UserForm.ts
+++ src/views/UserForm.ts
@@ -1,5 +1,7 @@
+import { User } from "../models/User";
+
 export class UserForm {
-  constructor(public parent: Element) {}
+  constructor(public parent: Element, public model: User) {}
 
   eventsMap(): { [key: string]: () => void } {
     return {
@@ -20,6 +22,12 @@ export class UserForm {
     return `
       <div>
         <h1>User Form</h1>
+        <div>
+          Name: ${this.model.get('name')}
+        </div>
+        <div>
+          Age: ${this.model.get('age')}
+        </div>
         <input />
         <button>click me</button>
       </div>
