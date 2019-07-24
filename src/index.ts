2899512a293c94b1cd57d10f8b71da64cc241845
Hubert Matwiejczyk <hmatwiejczyk@gmail.com>
2019-07-25 01:48:00 +0200

    add content to the dom

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,12 @@
-import { UserForm } from './views/UserForm';
+import { UserEdit } from './views/UserEdit';
 import { User } from './models/User';
 
 const user = User.createUser({ name: 'NAME', age: 20 });
 const root = document.getElementById('root');
 if (root) {
-  const userFrom = new UserForm(root, user);
-  userFrom.render();
+  const userEdit = new UserEdit(root, user);
+  userEdit.render();
+  console.log(userEdit);
 } else {
   throw new Error('no root element');
 }
