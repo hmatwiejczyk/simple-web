21f9680810ea54ed78d7f971cbb22f0207351966
Hubert Matwiejczyk <hmatwiejczyk@gmail.com>
2019-07-25 23:56:13 +0200

    add collections

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,16 @@
-import { UserEdit } from './views/UserEdit';
-import { User } from './models/User';
+import { UserList } from "./views/UserList";
+import { Collection } from "./models/Collection";
+import { UserProps, User } from "./models/User";
+import { baseUrl } from "./environment";
 
-const user = User.createUser({ name: 'NAME', age: 20 });
-const root = document.getElementById('root');
-if (root) {
-  const userEdit = new UserEdit(root, user);
-  userEdit.render();
-  console.log(userEdit);
-} else {
-  throw new Error('no root element');
-}
+const users = new Collection(baseUrl, (json: UserProps) => {
+  return User.createUser(json);
+});
+
+users.on('change', () => {
+  const root = document.getElementById('root');
+  if (root) {
+    new UserList(root, users).render();
+  }
+})
+users.fetch();
