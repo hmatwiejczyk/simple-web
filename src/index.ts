2f4ecf3a04eee076fc3cde6344f9e59eddfd7c2c
Hubert Matwiejczyk <hmatwiejczyk@gmail.com>
2019-07-25 23:59:10 +0200

    final test

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,27 @@
+import { UserEdit } from './views/UserEdit';
 import { UserList } from "./views/UserList";
 import { Collection } from "./models/Collection";
 import { UserProps, User } from "./models/User";
 import { baseUrl } from "./environment";
+
+const user = User.createUser({ name: 'NAME', age: 20 });
+const root = document.getElementById('form');
+if (root) {
+  const userEdit = new UserEdit(root, user);
+  userEdit.render();
+  console.log(userEdit);
+} else {
+  throw new Error('no root element');
+}
+
+
 
 const users = new Collection(baseUrl, (json: UserProps) => {
   return User.createUser(json);
 });
 
 users.on('change', () => {
-  const root = document.getElementById('root');
+  const root = document.getElementById('list');
   if (root) {
     new UserList(root, users).render();
   }
